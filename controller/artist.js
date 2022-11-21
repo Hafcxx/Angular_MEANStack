4f1978b54703ba9e1552448ba7e8822ee12f76da
Claudio Aninir <claudio.aninir01@gmail.com>
2022-11-21 15:28:01 -0300

    metodos de post y get para artistas creados, queda por corregir un error en el get artist

--- controller/artist.js
+++ controller/artist.js
@@ -7,9 +7,42 @@ const Album = require ('../models/album');
 const Song = require ('../models/song');
 
 function getArtist(req, res){
-    res.status(200).send({message : "working bitch"});
+    let artistId = req.params.artistId
+
+    Artist.findById(artistId, (err, artist) => {
+        if(err){
+            res.status(500).send({message : "Error al buscar artista"});
+        }else{
+            if(!artist){
+                res.status(404).send({message : "Artista no encontrado"});
+            }else{
+                res.status(200).send({artist});
+            }
+        }
+    })    
+}
+
+function saveArtist(req, res){
+    let artist = new Artist();
+    let params = req.body;
+    artist.name = params.name; 
+    artist.description = params.description;
+    artist.image = 'null';
+
+    artist.save((err, artistStored) => {
+        if(err){
+            res.status(500).send({message: 'Error al guardar artista'});
+        }else{
+            if(!artistStored){
+                res.status(404).send({message: 'El artista no ha sido guardado'});
+            }else{
+                res.status(200).send({artist: artistStored});
+            }
+        }
+    });
 }
 
 module.exports = {
-    getArtist
+    getArtist,
+    saveArtist
 }
--- routes/artist.js
+++ routes/artist.js
@@ -5,6 +5,7 @@ const artistController = require('../controller/artist');
 const api = express.Router();
 const md_auth = require('../middlewares/authenticated');
 
-api.get('/artist',md_auth.ensureAuth, artistController.getArtist);
+api.get('/artist/:id',md_auth.ensureAuth, artistController.getArtist);
+api.post('/artist',md_auth.ensureAuth, artistController.saveArtist);
 
 module.exports = api;
