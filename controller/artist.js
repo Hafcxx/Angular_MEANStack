e71f1072410347ee49cd2d0fd8297d53e4ffc7d3
Claudio Aniñir <claudio.aninir01@gmail.com>
2022-12-13 16:50:42 -0300

    paginación de artistas

--- controller/artist.js
+++ controller/artist.js
@@ -1,14 +1,38 @@
 'use strict'
 const path = require('path');
 const fs = require('fs');
+const mongoosePaginate  = require('mongoose-pagination');
 
 const Artist = require ('../models/artist');
 const Album = require ('../models/album');
 const Song = require ('../models/song');
 
-function getArtist(req, res){
-    let artistId = req.params.artistId
+function getArtists(req, res){
+    let page = 1
+    if(req.params.page){
+        page = req.params.page;
+    } 
+    let itemsPerPage = 3; 
 
+    Artist.find().sort('name').paginate(page, itemsPerPage, function (err, artists, total){
+        if (err){
+            res.status(500).send({message: 'Error en la petición.'});
+        }else {
+            if(!artists){
+                res.status(404).send({message: 'No hay artistas.'});
+            }else{
+                return res.status(200).send({
+                    total_items: total,
+                    artist: artists
+                });
+            }
+        }
+    });
+}
+
+function getArtist(req, res){
+    let artistId = req.params.id;
+    
     Artist.findById(artistId, (err, artist) => {
         if(err){
             res.status(500).send({message : "Error al buscar artista"});
@@ -44,5 +68,6 @@ function saveArtist(req, res){
 
 module.exports = {
     getArtist,
-    saveArtist
+    saveArtist,
+    getArtists
 }
--- routes/artist.js
+++ routes/artist.js
@@ -6,6 +6,8 @@ const api = express.Router();
 const md_auth = require('../middlewares/authenticated');
 
 api.get('/artist/:id',md_auth.ensureAuth, artistController.getArtist);
+api.get('/artists/:page?',md_auth.ensureAuth, artistController.getArtists);
+
 api.post('/artist',md_auth.ensureAuth, artistController.saveArtist);
 
 module.exports = api;
