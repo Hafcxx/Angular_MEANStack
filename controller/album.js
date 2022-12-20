bfc00dfbf7a55a8de8a59e626b0227407a308344
Claudio Aniñir <claudio.aninir01@gmail.com>
2022-12-20 17:19:57 -0300

    album con metodos de imagen

--- controller/album.js
+++ controller/album.js
@@ -8,6 +8,50 @@ const Album = require ('../models/album');
 const Song = require ('../models/song');
 const { restart } = require('nodemon');
 
+function uploadImage (req, res){
+    let albumId = req.params.id;
+    let file_name = 'No subido...';
+
+    if(req.files){
+        let file_path = req.files.image.path; 
+        let file_split = file_path.split('\\')
+        let file_name = file_split[2];
+        let ext_split = file_name.split('\.')
+        let file_ext = ext_split[1]
+
+        if(file_ext == 'png' || file_ext == 'jpg' || file_ext == 'gif' ){
+
+            Album.findByIdAndUpdate(albumId, {image: file_name}, (err, albumUpdate) =>{
+                if (!albumUpdate ){
+                        res.status(404).send ({message: 'No se ha podido acutalizar el usuario'});
+                }else{
+                    if(err){
+                        res.status(500).send (err);
+                    }else{
+                    res.status(200).send({album: albumUpdate})}
+                }
+            });
+
+        }else {
+            res.status(200).send({message: 'Extensión del archivo no valida'})
+        }
+    }else{
+        req.status(200).send({message: 'No has subido ninguna imagen...'})
+    }
+}
+
+function getImageFile(req, res){
+    let imageFile = req.params.imageFile;
+    let path_file = './uploads/albums/'+imageFile
+    fs.exists(path_file, function(exists){
+        if(exists){
+            res.sendFile(path.resolve(path_file));
+        }else{
+            res.status(200).send({message: 'No existe la imagen'});
+        }
+    });
+}
+
 function updateAlbum (req, res){
     let albumId = req.params.id;
     let update = req.body; 
@@ -117,5 +161,7 @@ module.exports = {
     saveAlbum,
     getAlbums,
     updateAlbum,
-    deleteAlbum
+    deleteAlbum,
+    uploadImage,
+    getImageFile
 };
--- routes/album.js
+++ routes/album.js
@@ -6,14 +6,18 @@ const api = express.Router();
 const md_auth = require('../middlewares/authenticated');
 
 const multipart = require ('connect-multiparty');
-const md_upload = multipart ({ uploadDir: './uploads/album'})
+const md_upload = multipart ({ uploadDir: './uploads/albums'})
 
 api.get('/album/:id',md_auth.ensureAuth, albumController.getAlbum);
 api.get('/albums/:artist?',md_auth.ensureAuth, albumController.getAlbums);
+api.get('/get-image-album/:imageFile', albumController.getImageFile);
 
+api.post('/upload-image-album/:id', [md_auth.ensureAuth, md_upload], albumController.uploadImage);
 api.post('/album',md_auth.ensureAuth, albumController.saveAlbum);
 api.put('/album/:id',md_auth.ensureAuth, albumController.updateAlbum);
 
 api.delete('/album/:id',md_auth.ensureAuth, albumController.deleteAlbum);
+
+
 
 module.exports = api;
