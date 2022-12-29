badbe6cb28bb2cabed53055296a891559a3b117f
Claudio Aniñir <claudio.aninir01@gmail.com>
2022-12-29 11:38:53 -0300

    login y registro completados

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -13,21 +13,28 @@ export class AppComponent implements OnInit{
   public title = 'Musify';
   public user: User;
   public identity: any; 
-  public token;
+  public token: any;
   public errorMessage = "";
+  public errorRegister = "";
+  public user_register: User;
 
   constructor (
     private _userService: UserService
   ){
     this.user = new User ('', '', '', '', '' ,'ROLE_USER','')
+    this.user_register = new User ('', '', '', '', '' ,'ROLE_USER','')
     this.identity; 
-    this.token = "";
+    this.token;
     this.errorMessage;
-
+    this.errorRegister;
   }
 
   ngOnInit(){
+    this.identity = this._userService.getIdentity();
+    this.token = this._userService.getToken();
 
+    console.log(this.identity);
+    console.log(this.token);
   }
 
   public onSubmit(){
@@ -44,7 +51,7 @@ export class AppComponent implements OnInit{
           alert("El usuario no está correctamente identificado");
         }else {
           //Crear elemento en el local storage
-          
+          localStorage.setItem('identity',JSON.stringify(identity));
           //Conseguir el token para enviarselo a cada petición http
                   this._userService.signUp(this.user, 'true').subscribe({
                     next: (v:any) => {
@@ -56,7 +63,7 @@ export class AppComponent implements OnInit{
                       alert("El token esta mal");
                     }else {
                       //Crear elemento en el local storage para el token
-
+                      localStorage.setItem('token',token);
                       console.log(token);
                       console.log(identity);
             
@@ -81,4 +88,37 @@ export class AppComponent implements OnInit{
       }
     )
   }
+
+  logout (){
+    localStorage.removeItem('identity');
+    localStorage.removeItem('token');
+    localStorage.clear();
+    this.identity = null;
+    this.token = null;
+  }
+  
+  onSubmitRegister(){
+    console.log(this.user_register);
+
+    this._userService.register(this.user_register).subscribe({
+      next: (v:any) => {
+        let user = v.user;
+        this.user_register = user;
+
+        if(!user._id){
+          alert('Error al registrarse');
+          this.errorRegister = 'Error al registrarse';
+        }else {
+          this.errorRegister = 'El registro se ha realizado correctamente, identificate con: ' + this.user_register.email;
+          this.user_register = new User ('', '', '', '', '' ,'ROLE_USER','');
+        }
+      },
+      error: (e)=>{
+        this.errorRegister = e.error.message;
+      },
+      complete: ()=> {}
+
+    })
+  }
+
 }
--- client/src/app/services/user.service.ts
+++ client/src/app/services/user.service.ts
@@ -27,6 +27,30 @@ export class UserService{
         });
 
         return this._http.post(this.url+'login', params, {headers: headers})
-                        .pipe(map((res) => res))
+                        .pipe(map((res) => res));
     }
+
+    register (user_to_register: object){
+
+        let params = JSON.stringify(user_to_register);
+
+        let headers = new HttpHeaders({ 'Content-Type':'application/json'});
+
+        return this._http.post(this.url+'register', params, {headers: headers})
+                        .pipe(map((res) => res));
+    }
+
+    getIdentity (){
+        let identityString = localStorage.getItem('identity')+"";
+        let identity;
+        if (identityString != ""){
+          identity = JSON.parse(identityString);
+        }
+        return identity;
+      }
+    
+      getToken(){
+        let tokenString = localStorage.getItem('token')+"";
+        return tokenString;
+      }
 };
