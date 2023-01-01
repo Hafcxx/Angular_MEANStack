f0958446b0190d772871cd4aa81b4577cc11cf2f
Claudio Aniñir <claudio.aninir01@gmail.com>
2023-01-01 18:12:18 -0300

    actualizar imagenes de usuario completado

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from './services/user.service';
 import { User} from './models/user';
+import { GLOBAL } from './services/global';
 
 @Component({
   selector: 'app-root',
@@ -17,6 +18,7 @@ export class AppComponent implements OnInit{
   public errorMessage = "";
   public errorRegister = "";
   public user_register: User;
+  public url: string; 
 
   constructor (
     private _userService: UserService
@@ -27,6 +29,7 @@ export class AppComponent implements OnInit{
     this.token;
     this.errorMessage;
     this.errorRegister;
+    this.url = GLOBAL.url;
   }
 
   ngOnInit(){
--- client/src/app/components/user-edit.component.ts
+++ client/src/app/components/user-edit.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../services/user.service';
 import { User } from '../models/user';
+import {GLOBAL} from '../services/global';
 
 @Component({
     selector: 'user-edit',
@@ -15,6 +16,8 @@ export class UserEditComponent implements OnInit{
     public token;
     public alertUpdate = "";
     public errorMessage: any;
+    public filesToUpload: Array<File>;
+    public url: string; 
 
     constructor(
         private _userService: UserService
@@ -27,6 +30,9 @@ export class UserEditComponent implements OnInit{
         this.titulo = ""; 
         this.errorMessage;
 
+        this.filesToUpload =[];
+
+        this.url = GLOBAL.url;
         // LocalStorage
 
     }
@@ -46,8 +52,25 @@ export class UserEditComponent implements OnInit{
                 if(!v.user){
                     this.errorMessage = 'El usuario no se ha actualizado';
                 }else {
+
                     this.user = v.user; 
                     localStorage.setItem('identity', JSON.stringify(v.user));
+
+                    if(!this.filesToUpload){
+                        //Redireccion
+                    }else{
+                        this.makeFileRequest(this.url+'upload-image-user/'+this.user._id, [], this.filesToUpload)
+                        .then( (result: any) =>{
+                            this.user.image = result.image;
+                            localStorage.setItem('identity', JSON.stringify(v.user));
+
+                            let image_path=this.url+'get-image-user/'+this.user.image;
+                            document.getElementById('user_image')?.setAttribute('src',image_path);
+                            console.log(this.user);
+                        });
+                    }
+
+                    
                     this.errorMessage = 'actualizado correctamente';
                 }
             },
@@ -56,6 +79,41 @@ export class UserEditComponent implements OnInit{
                 console.log(e);
             },
             complete: () => {}
+        });
+    }
+
+    
+
+
+    fileChangeEvent(fileInput: any){
+        this.filesToUpload = <Array<File>>fileInput.target.files;
+        console.log(this.filesToUpload);
+    }
+
+    makeFileRequest(url: string, params: Array<string>, files: Array<File>){
+        var token = this.token;
+
+        return new Promise(function(resolve, reject){
+            var formData:any = new FormData();
+            var xhr = new XMLHttpRequest();
+            //creamos un formulario xml para enviar archivos en nuestra peticion
+            for(var i= 0; i < files.length; i++){
+                formData.append('image', files[i], files[i].name);
+            }
+            
+            xhr.onreadystatechange = function(){
+                if(xhr.readyState == 4){
+                    if(xhr.status == 200){
+                        resolve(JSON.parse(xhr.response));
+                    }else{
+                        reject(xhr.response);
+                    } 
+                }
+            }
+
+            xhr.open('POST', url, true);
+            xhr.setRequestHeader('Authorization', token);
+            xhr.send(formData);
         });
     }
 }
